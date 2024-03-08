2939ab3e80a340a5adc5080c0313a42c09a78099
lko9911 <160494158+lko9911@users.noreply.github.com>
2024-03-08 12:13:52 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/25304. 영수증/영수증.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int X, N, a, b, sum=0;
+	cin >> X >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> a >> b;
+		sum += a * b;
+	}
+	if (sum == X)
+		cout << "Yes";
+	else
+		cout << "No";
+}
