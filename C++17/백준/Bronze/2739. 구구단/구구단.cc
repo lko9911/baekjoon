c296de845eab99ad91b6466fd8f5da4d22b6a15d
lko9911 <160494158+lko9911@users.noreply.github.com>
2024-03-17 19:56:31 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2739. 구구단/구구단.cc
+++ C++17/백준/Bronze/2739. 구구단/구구단.cc
@@ -2,9 +2,8 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-
-    for (int i = 1; i <= 9; i++)
-        cout << n << " * " << i << " = " << n * i << endl;
+	int n;
+	cin >> n;
+	for (int i = 1; i < 10; i++)
+		cout << n << " * " << i << " = " << n * i << endl;
 }
