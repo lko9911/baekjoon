88e9fed67cba708e1ef0581b8fd2a3e4382ca7e7
lko9911 <160494158+lko9911@users.noreply.github.com>
2024-04-03 23:55:32 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string a, fan = ":fan:";
+	cin >> a;
+	cout << fan << fan << fan << endl;
+	cout << fan << ':' << a << ':' << fan << endl;
+	cout << fan << fan << fan << endl;
+}
